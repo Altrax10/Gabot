5ef21c449a3b82415a988b0606afb983d7ac9dcf
Altrax10 <54655130+Altrax10@users.noreply.github.com>
2022-11-13 09:57:10 +0700

    Update echo.py

--- echo.py
+++ echo.py
@@ -4,6 +4,7 @@ from discord.ext import commands
 async def setup(bot):
     await bot.add_cog(echo(bot))
    
+@commands.command()
 async def echo(self,ctx):
     client = self.client
     msg = ctx.message.content
