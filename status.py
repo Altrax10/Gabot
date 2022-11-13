a94225c67d48278fc7a4418805abd3cb67d85901
Altrax10 <54655130+Altrax10@users.noreply.github.com>
2022-11-13 09:09:30 +0700

    Update status.py

--- status.py
+++ status.py
@@ -35,4 +35,4 @@ class status(commands.Cog):
         await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="OnePiece"))
         
 def setup(client):
-  client.add_cog(status(client))
+  await client.add_cog(status(client))
