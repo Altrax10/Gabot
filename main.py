983684476dd7b1c04f825eaed3efc38033ea13eb
Altrax10 <54655130+Altrax10@users.noreply.github.com>
2022-11-13 10:11:44 +0700

    Update main.py

--- main.py
+++ main.py
@@ -131,7 +131,7 @@ async def purge(ctx, limit: int):
     await ctx.channel.purge(limit=limit)
 
 @app_commands.command(name="klaim", description="Buat Klaim Badge Dev")
-async def klaim(client, interaction: discord.Interaction):
+async def klaim(client, interaction:discord.interaction):
   await interaction.response.send_message(f"Cek Statusmu Disini")
     
 
