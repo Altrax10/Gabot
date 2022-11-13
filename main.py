b5e25e780072982675b19ff9c8132dbd5645a16e
Altrax10 <54655130+Altrax10@users.noreply.github.com>
2022-11-13 09:31:34 +0700

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ TOKEN = "NzQ5OTYzMTkxNTg4NDg3MjQ4.GVNSaf.SIA6heSx2u5W7TBPeo3hjSZgn5tZOl-TP8QQwo"
 client = commands.Bot(command_prefix = '$', intents = discord.Intents.all())
 
 async def load_extensions():
-    for filename in os.listdir("./cogs"):
+    for filename in SETUP("./cogs"):
         if filename.endswith(".py"):
             # cut off the .py from the file name
             await client.load_extension(f"cogs.{filename[:-3]}")
