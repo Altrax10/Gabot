0f206dc9b69cac2e3c91abfd0635c893c9e7fcab
Altrax10 <54655130+Altrax10@users.noreply.github.com>
2022-11-16 18:03:37 +0700

    Update main.py

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ async def klaim(interaction : discord.Interaction):
   
 @client.command()
 async def sync(ctx):
-  await ctx.client.tree.sync(guild = discord.Object(id=526100423250149386))
+  await ctx.bot.tree.sync(guild = discord.Object(id=526100423250149386))
   await ctx.send("synced")
  
 @client.event
