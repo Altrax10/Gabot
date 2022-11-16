43569e87529d67bfbdfa745036b902827cfa865e
Altrax10 <54655130+Altrax10@users.noreply.github.com>
2022-11-16 17:10:23 +0700

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ import interactions
 
 cogs=[status,musik,valo,echo,gi,pagi,afk]
 TOKEN = "NzQ5OTYzMTkxNTg4NDg3MjQ4.GVNSaf.SIA6heSx2u5W7TBPeo3hjSZgn5tZOl-TP8QQwo"
-client = commands.Bot(command_prefix = '$', intents = discord.Intents.all())
+client = commands.Bot(command_prefix = '$')
 
 #for i in range(len(cogs)):
 #  cogs[i].setup(client)
