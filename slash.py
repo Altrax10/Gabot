4b993f7792f2bfe38eefbdb2af31518e64d0bd9d
Altrax10 <54655130+Altrax10@users.noreply.github.com>
2022-11-17 15:33:42 +0700

    Update slash.py

--- slash.py
+++ slash.py
@@ -14,15 +14,11 @@ class slash(commands.Cog):
     MB = KB * 1024 # One MB is 1024 KB
     return int(bytes/MB)
 
-  async def kecepatan():
-    
-    download_speed = bytes_to_mb(speed_test.download())
-    upload_speed = bytes_to_mb(speed_test.upload())
+download_speed = bytes_to_mb(speed_test.download())
+upload_speed = bytes_to_mb(speed_test.upload())
 
-    return download_speed
-    return upload_speed
 
-  @app_commands.command(name="ping")
+  @app_commands.command(name="ping", description ="Speed Test")
   async def ping(self, interaction: discord.Interaction) -> None:
 
     #await interaction.response.defer(ephemeral = True)
