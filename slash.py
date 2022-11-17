68f95f7b8511239cc44e100e9e85233b8324223f
Altrax10 <54655130+Altrax10@users.noreply.github.com>
2022-11-17 14:10:42 +0700

    Update slash.py

--- slash.py
+++ slash.py
@@ -12,7 +12,7 @@ class slash(commands.Cog):
   def bytes_to_mb(bytes):
     KB = 1024 # One Kilobyte is 1024 bytes
     MB = KB * 1024 # One MB is 1024 KB
-  return int(bytes/MB)
+    return int(bytes/MB)
 
   download_speed = bytes_to_mb(speed_test.download())
   upload_speed = bytes_to_mb(speed_test.upload())
