248a428f1c4c7ef5486d1364ddc65acc522ef8c3
Altrax10 <54655130+Altrax10@users.noreply.github.com>
2022-11-17 13:08:07 +0700

    Create slash.py

---
+++ slash.py
@@ -0,0 +1,15 @@
+import discord
+from discord import app_commands
+from discord.ext import commands
+
+class echo(commands.Cog):
+  def __init__ (self, bot: commands.Bot ):
+    self.bot = bot
+    
+  @app_commands.command(name="command-1")
+  async def my_command(self, interaction: discord.Interaction) -> None:
+    """ /command-1 """
+    await interaction.response.send_message("Hello from command 1!", ephemeral=True)
+   
+async def setup(bot):
+    await bot.add_cog(echo(bot))
