f8ebac43eb9c35010979fb00d203907dae607c49
Altrax10 <54655130+Altrax10@users.noreply.github.com>
2022-11-17 15:21:59 +0700

    Update slash.py

--- slash.py
+++ slash.py
@@ -6,22 +6,24 @@ import speedtest
 class slash(commands.Cog):
   def __init__ (self, bot: commands.Bot ):
     self.bot = bot
+ 
+  speed_test = speedtest.Speedtest()
+
+  async def bytes_to_mb(bytes):
+    KB = 1024 # One Kilobyte is 1024 bytes
+    MB = KB * 1024 # One MB is 1024 KB
+    return int(bytes/MB)
+
+  global download_speed = bytes_to_mb(speed_test.download())
+  global upload_speed = bytes_to_mb(speed_test.upload())
   
   @app_commands.command(name="ping")
   async def ping(self, interaction: discord.Interaction) -> None:
-    speed_test = speedtest.Speedtest()
 
-    async def bytes_to_mb(bytes):
-      KB = 1024 # One Kilobyte is 1024 bytes
-      MB = KB * 1024 # One MB is 1024 KB
-      return int(bytes/MB)
-
-    download_speed = bytes_to_mb(speed_test.download())
-    upload_speed = bytes_to_mb(speed_test.upload())
-    await interaction.response.defer(ephemeral = True)
-    await asyncio.sleep(4)
-    await interaction.followup.send('yo')
-    #await interaction.response.send_message(f"Download = {download_speed}n/ Upload = {upload_speed}")
+    #await interaction.response.defer(ephemeral = True)
+    #await asyncio.sleep(4)
+    #await interaction.followup.send('yo')
+    await interaction.response.send_message(f"Download = {download_speed}n/ Upload = {upload_speed}")
    
 async def setup(bot):
     await bot.add_cog(slash(bot))
