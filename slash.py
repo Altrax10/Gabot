42e931353eb95c102e22a3d7fe632681a577e824
Altrax10 <54655130+Altrax10@users.noreply.github.com>
2022-11-17 14:27:58 +0700

    Update slash.py

--- slash.py
+++ slash.py
@@ -9,15 +9,15 @@ class slash(commands.Cog):
   
   @app_commands.command(name="ping")
   async def ping(self, interaction: discord.Interaction) -> None:
-      speed_test = speedtest.Speedtest()
+    speed_test = speedtest.Speedtest()
 
-      def bytes_to_mb(bytes):
-        KB = 1024 # One Kilobyte is 1024 bytes
-        MB = KB * 1024 # One MB is 1024 KB
-        return int(bytes/MB)
+    def bytes_to_mb(bytes):
+      KB = 1024 # One Kilobyte is 1024 bytes
+      MB = KB * 1024 # One MB is 1024 KB
+      return int(bytes/MB)
 
-      download_speed = bytes_to_mb(speed_test.download())
-      upload_speed = bytes_to_mb(speed_test.upload())
+    download_speed = bytes_to_mb(speed_test.download())
+    upload_speed = bytes_to_mb(speed_test.upload())
     await interaction.response.send_message(f"Download = {download_speed}n/ Upload = {upload_speed}")
    
 async def setup(bot):
