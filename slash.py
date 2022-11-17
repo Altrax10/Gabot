33b4601b324f855c121a4fd727952cc462f631c8
Altrax10 <54655130+Altrax10@users.noreply.github.com>
2022-11-17 15:01:00 +0700

    Update slash.py

--- slash.py
+++ slash.py
@@ -19,8 +19,8 @@ class slash(commands.Cog):
     download_speed = bytes_to_mb(speed_test.download())
     upload_speed = bytes_to_mb(speed_test.upload())
     await interaction.response.defer(ephemeral = True)
-    await asyncio.sleep(5)
-    await interaction.followup.send(f"Download = {download_speed}n/ Upload = {upload_speed}")
+    await asyncio.sleep(4)
+    await interaction.followup.send('yo')
     #await interaction.response.send_message(f"Download = {download_speed}n/ Upload = {upload_speed}")
    
 async def setup(bot):
